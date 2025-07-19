e0faa7472cfbeae9e92f5f5b642945ade0e8a186
YASHPATEL\patel <patelyash2462@gmail.com>
2025-07-19 21:22:34 +0530

    reverse string using recursion

--- edabit_questions/ReverseStringRecursion.java
+++ edabit_questions/ReverseStringRecursion.java
@@ -1,9 +1,16 @@
 package edabit_questions;
 
 public class ReverseStringRecursion {
-//    public static String reverse(String str) {
-//      String ans ="";
-//
-//
-//    }
+    public static String reverse(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String name = "yash";
+        System.out.println("name :"+name);
+        System.out.println("reversed name :"+reverse(name));
+    }
 }
